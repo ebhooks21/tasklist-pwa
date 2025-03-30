f9469b7858c390e5e0525875b69d5f9e2bf8fda8
Eric Hooks <ebhooks21@gmail.com>
2025-03-29 20:19:30 -0500

    Setup a array for the task list.

---
+++ js/app/App.js
@@ -0,0 +1,17 @@
+/**
+ * App.js -- File for the main App class.
+ * Author: Eric Hooks 
+ * Date: 03/29/2025
+ */
+
+class App {
+	/**
+	 * Main constructor for the App class.
+	 */
+	constructor() {
+		let self = this;
+
+		//Define an array for the tasks
+		self.taskList = [];
+	}
+}
--- js/app/Main.js
+++
@@ -1,4 +0,0 @@
-function start() {
-	console.log(window.localStorage.getItem('name'));
-	window.localStorage.setItem('name', 'Larry2');
-}
